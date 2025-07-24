9f52de27f545342787b2f800109425625979cd16
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-24 14:12:23 +0300

    creating packages

---
+++ LearningManagementSystem/src/main/java/com/spring/boot/learningmanagementsystem/ApiResponse/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.spring.boot.learningmanagementsystem.ApiResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+}
