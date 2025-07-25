835d4ae934e66078b8fa5f05a3a15f121be0a054
Abdullah Alwael <abdullah.alwael@outlook.sa>
2025-07-25 19:42:33 +0300

    validated the Instructor model

--- LearningManagementSystem/src/main/java/com/spring/boot/learningmanagementsystem/Model/Instructor.java
+++ LearningManagementSystem/src/main/java/com/spring/boot/learningmanagementsystem/Model/Instructor.java
@@ -1,15 +1,29 @@
 package com.spring.boot.learningmanagementsystem.Model;
 
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Instructor {
+
+    @NotEmpty(message = "ID can not be empty")
     private String iD;
+
+    @NotEmpty(message = "name can not be empty")
+    @Pattern(regexp = "[a-zA-Z ]", message = "name must not have numbers or any special characters")
     private String name;
+
+    // can be empty
     private String currentClass;
     private String currentStudentQuestion;
+
+    @AssertFalse(message = "The teacher can not start teaching before attending a class")
     private boolean isTeaching;
+
+    @AssertFalse(message = "The teacher can not start marking attendance before attending a class")
     private boolean hasMarkedAttendance;
 }
